6fc43d18699c9f7d6f9cd9fe6496b5a5d22bafe1
AEMY Studio <noreply@aemy.studio>
2026-03-31 20:59:50 +0000

    Add frescopa-banner block
    Automated migration updates generated by Experience Modernization Agent.
    
    Changed files (1):
    - blocks/frescopa-banner/frescopa-banner.js

--- blocks/frescopa-banner/frescopa-banner.js
+++ blocks/frescopa-banner/frescopa-banner.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
+  // eslint-disable-next-line no-console
+  console.log('Frescopa banner block loaded:', block);
   // Frescopa banner layout handled entirely by CSS
 }
